90ea8b70a2b6a931475167ce2eae52631a3483c1
hassandiab121 <diabe057@gmail.com>
2026-06-02 09:50:02 +0300

    initial configuration

---
+++ src/main/java/config/Security.java
@@ -0,0 +1,16 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class Security {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
